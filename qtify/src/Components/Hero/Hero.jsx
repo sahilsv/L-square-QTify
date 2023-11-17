de13b5172b3d5dd393442ad9c5b13ae8228ab2ad
Sahil Singh Virdhi <sahilsv003@gmail.com>
2023-11-17 12:52:59 +0530

    Hero Section

--- qtify/src/Components/Hero/Hero.jsx
+++ qtify/src/Components/Hero/Hero.jsx
@@ -1,13 +1,21 @@
-import React from 'react'
-import './hero.css';
-import hero from '../../Assets/hero.png'
+import React from "react";
+import "./hero.css";
+import headphones from "../../Assets/headphones.png";
 
 function Hero() {
   return (
-    <div className='hero'>
-        <img src={hero} alt="Hero img not available" />
+    <div className="hero">
+      <div className="heading">
+        <h1>100 Thousand Songs, ad-free</h1>
+        <h1>Over thousands podcast episodes</h1>
+      </div>
+      <img
+        className="headphone"
+        src={headphones}
+        alt="headphones img not available"
+      />
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
